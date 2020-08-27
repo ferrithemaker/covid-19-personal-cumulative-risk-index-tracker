7e85581fadd7bbc610f2b2cddd1fefb5a5a37035
Ferran <ferri.fc@gmail.com>
2020-08-27 21:53:04 +0200

    smartband version

--- README.md
+++ README.md
@@ -8,22 +8,21 @@ The covid-19 personal cumulative risk index tracker is a wireless device that is
 
 Actually, almost everyone wears a device with a wireless connection on them, and every device with this type of connection, even if it is not connected to any network or in use, regularly emits a series of beacon signals to identify itself in a unique way.
 
-The covid-19 personal cumulative risk index tracker recognizes these signals and their intensity, which allow us to know approximately the distance from the source, and based on that information creates a daily cumulative index of exposure risk, like the radiation dosimeters.
+The covid-19 personal cumulative risk index tracker recognizes these signals and their intensity, density and variability, which allow us to know approximately the flow of people around you and based on that information, plus a real-time data of local infection cases, the device creates a reliable daily cumulative index of exposure risk, like the radiation dosimeters.
 
-The central point of the project is not the hardware itself, quite simple indeed, but the algorithm that calculates this index based on the electromagnetic frequency readings. The idea is not simply to detect the number of devices around you, but to analyze the variations of the signals intensity,  and above all, analyze the flow of devices around you.
+The central point of the project is not the hardware itself, quite simple indeed, but the algorithm that calculates this index based on the electromagnetic frequency readings. The idea is not simply to detect the number of devices around you, but to analyze the variations of the signals intensity,  and above all, analyze the flow of devices around you. 
 
 These data allow us to calculate what kind of environment we are in, generating various types of profiles:
 
 - Very dense environment and low variability: (moderate / high risk)
-
 - Very dense environment and high variability: (high / very high risk)
-
 - Low dense environment and low variability: (low risk)
-
 - Low dense environment and high variability: (moderate risk)
+- Based on these profiles we can calculate a daily accumulated index that will tell us not only if at a specific moment we are in a potentially dangerous environment (since this is generally quite obvious), but it will also show us an accumulated value throughout the day, which will give us a global assessment of our daily behaviour (low risk / medium risk / high risk) and will allow us to be aware in order to adjust it and minimize further risks.
 
-Based on these profiles we can calculate a daily accumulated index that will tell us not only if at a specific moment we are in a potentially dangerous environment (since this is generally quite obvious), but it will also show us an accumulated value throughout the day, which will give us a global assessment of our daily behaviour (low risk / medium risk / high risk) and will allow us to be aware in order to adjust it and minimize further risks.
+This index can be weighted with real time data of local infections rates from online oficial sources like covid-19 API to create a more reliable index.
 
 The device consists of a wireless connectivity microcontroller with the ability to work in promiscuous mode (in our case an ESP32) and a battery (a small power bank or a LiPo battery). To display the information you can use an OLED screen (this is the case in the demo example) or simply a color coding system using LEDs to indicate the different levels of accumulated risk.
 
-With all these elements, we can create a wearable device that can be stored on our pocket or backpack, maybe in the form of a watch in our wrist or as a clothing accessory like a classic dosimeter. The final design of the device is up to you, your skills and your creativity! 
+
+
---
+++ main-ttgo-wristband.cpp
@@ -0,0 +1,261 @@
+#include <Arduino.h>
+
+#include "WiFi.h"
+#include "esp_wifi.h"
+#include <HTTPClient.h>
+#include <vector>
+#include "string.h"
+
+#include <SPI.h>
+
+#include <TFT_eSPI.h> // Hardware-specific library
+// VERY IMPORTANT!!! 
+// You need to modify the library file User_Setup_Select.h
+// comment the line #include <User_Setup.h>
+// uncomment the line #include <User_Setups/Setup26_TTGO_T_Wristband.h>
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+
+class MACPool {
+private:
+ String mac;
+ int signal;
+ unsigned long time;
+ bool newMAC;
+ 
+ 
+public:
+MACPool(String,int,unsigned long,bool);
+String getMAC();
+int getSignal();
+unsigned long getTime();
+void updateTime(unsigned long);
+bool getNewMAC();
+void updateNewMAC(bool);
+};
+
+MACPool::MACPool(String mac,int signal,unsigned long time,bool newMAC)
+{
+ this->mac = mac;
+ this->signal = signal;
+ this->time = time;
+ this->newMAC = newMAC;
+}
+
+String MACPool::getMAC() {
+  return this->mac;
+}
+
+int MACPool::getSignal() {
+  return this->signal;
+}
+
+unsigned long MACPool::getTime() {
+  return this->time;
+}
+
+void MACPool::updateTime(unsigned long time) {
+  this->time=time;
+}
+
+void MACPool::updateNewMAC(bool nm) {
+  this->newMAC=nm;
+}
+
+bool MACPool::getNewMAC() {
+  return this->newMAC;
+}
+
+
+using namespace std;
+
+const char* ssid     = "xxxx";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "xxxx";         // network password
+
+unsigned int channel = 1;
+
+float weight = 1.0; // weigh the index with real time data
+
+
+const wifi_promiscuous_filter_t filt={
+    .filter_mask=WIFI_PROMIS_FILTER_MASK_MGMT|WIFI_PROMIS_FILTER_MASK_DATA
+};
+
+
+vector<MACPool> listOfMAC;
+
+int riskIndex = 0;
+String riskValue = "low";
+
+typedef struct {
+  uint8_t mac[6];
+} __attribute__((packed)) MacAddr;
+
+typedef struct {
+  int16_t fctl;
+  int16_t duration;
+  MacAddr da;
+  MacAddr sa;
+  MacAddr bssid;
+  int16_t seqctl;
+  unsigned char payload[];
+} __attribute__((packed)) WifiMgmtHdr;
+
+void sniffer(void* buf, wifi_promiscuous_pkt_type_t type) {
+  
+    wifi_promiscuous_pkt_t *p = (wifi_promiscuous_pkt_t*)buf;
+    WifiMgmtHdr *wh = (WifiMgmtHdr*)p->payload;
+    int signal = p->rx_ctrl.rssi;
+
+    MacAddr mac_add = (MacAddr)wh->sa;
+    String sourceMac;
+    for (int i = 0; i < sizeof(mac_add.mac); i++) {
+        String macDigit = String(mac_add.mac[i], HEX);
+        if (macDigit.length() == 1) {
+            macDigit = "0" + macDigit;
+        }
+        
+        sourceMac += macDigit;
+        if (i != sizeof(mac_add.mac) - 1) {
+          sourceMac += ":";
+        }
+    }
+
+    sourceMac.toUpperCase();
+
+    if (signal > -70) { // signal level threshold
+
+      // Prevent duplicates
+      for (int i = 0; i < listOfMAC.size(); i++) {
+          if (sourceMac == listOfMAC[i].getMAC()) {
+              listOfMAC[i].updateTime(millis()); // update the last time MAC found
+              listOfMAC[i].updateNewMAC(false);
+              return;
+          }
+      }
+
+      // new MAC
+
+      listOfMAC.push_back(MACPool(sourceMac,signal,millis(),true));
+
+      Serial.println(listOfMAC[listOfMAC.size()-1].getMAC());
+
+      // purge outdated MACs
+
+      for (auto it = listOfMAC.begin(); it != listOfMAC.end(); ) {
+          if (millis()-it->getTime() > 300000) { // remove if older than 5min
+              it = listOfMAC.erase(it);
+          } else {
+              ++it;
+          }
+      }
+
+      // update the risk index
+
+      int recentLowSingal = 0;
+      int recentHighSingal = 0;
+      for (int i = 0; i < listOfMAC.size(); i++) {
+          if (millis()-listOfMAC[i].getTime() < 30000 && listOfMAC[i].getNewMAC()==true) {
+              if (listOfMAC[i].getSignal() < -60) { recentLowSingal++; } else { recentHighSingal++; }
+          }
+          // new low and high signals from last 30sec
+      }
+
+      riskIndex = int(riskIndex + recentLowSingal + (2*recentHighSingal) * weight);
+
+      // initial estimation -provisional- : activities with moderated risk: 800-1300 / hour
+      // activities with low risk: < 800 / hour
+      // aftivities with high risk: > 1300 / hour
+      // if you are using the device 12 hour / day: the risk is low if < 9600, medium if >= 9600 <= 15600, high > 15600
+      if (riskIndex<9600) { riskValue="low";}
+      if (riskIndex>=9600 && riskIndex<=15600) { riskValue="medium";}
+      if (riskIndex>15600) { riskValue="high, go home, please!"; }
+
+    }
+}
+
+
+void setup(void) {
+  tft.init();
+  tft.setRotation(2);
+
+  /*
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); 
+  Serial.println(" ...");
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED && i < 50) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); 
+    Serial.print(' ');
+  }
+  
+  if (i < 50) { // connection OK
+    Serial.println('\n');
+    Serial.println("Connection established!");  
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+    HTTPClient client;
+    client.begin("http://api.ferranfabregas.me/covid19"); // not the definitive API, for testing only
+    int httpCode = client.GET();
+    if (httpCode > 0) {
+      // only for testing
+      String payload = client.getString(); // get the data
+      //Serial.println(payload);
+      int inf_pos = payload.indexOf("infected");
+      String infected = payload.substring(inf_pos+11,payload.indexOf(",",inf_pos));
+      int inf_inh = payload.indexOf("inhabitants");
+      String inhabitants = payload.substring(inf_inh+14,payload.indexOf(",",inf_inh));
+      //Serial.println(inhabitants);
+      weight = (infected.toFloat() / inhabitants.toFloat()) * 100;
+      Serial.println(weight);
+    } else {
+      Serial.println("Error on HTTP request");
+    }
+  } else { 
+    Serial.println("failed!");
+    delay(2000);
+  }
+  WiFi.disconnect();
+  WiFi.enableSTA(false);
+  WiFi.softAPdisconnect(true);
+  delay(5000);
+  */
+
+  Serial.println("Setting promiscuous mode...");
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  esp_wifi_init(&cfg);
+  // set WiFi in promiscuous mode
+  //esp_wifi_set_mode(WIFI_MODE_STA);            // Promiscuous works only with station mode
+  esp_wifi_set_mode(WIFI_MODE_NULL);
+  esp_wifi_start();
+  esp_wifi_set_storage(WIFI_STORAGE_RAM);
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_promiscuous_filter(&filt);
+  esp_wifi_set_promiscuous_rx_cb(&sniffer);   // Set up promiscuous callback
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+}
+
+void loop() {
+  
+  tft.fillScreen(0x000000);
+  
+  tft.setCursor(0, 0, 2);
+  tft.setTextColor(TFT_WHITE,TFT_BLACK);  
+  tft.setTextSize(1);
+  // We can now plot text on screen using the "print" class
+  tft.println("Covid-19");
+  tft.println("daily");
+  tft.println("accumulated");
+  tft.println("risk index");
+  tft.println("");
+  tft.setTextFont(4);
+  tft.println(riskValue);
+  tft.println((String)riskIndex);
+  channel++;
+  if(channel == 11) channel = 1;
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  delay(1000);
+}
